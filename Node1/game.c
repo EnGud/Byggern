900e8350bb5fbfd6050a5e5e2b9ea3774fde94c0
EnGud <60407025+EnGud@users.noreply.github.com>
2020-11-17 10:32:14 +0100

    Update game.c

--- Node1/game.c
+++ Node1/game.c
@@ -1,1 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <avr/io.h>
+#include "oled.h"
+#include "CAN.h"
 
+void setup(void){}
+
+
+void main(void){
+
+  void PlayGame(void){
+    while(Game=="True"){
+      can_send(joystick)
+      CAN_recieve(somestruct)
+        
+    }
+  }
+  
+  void ShowHighScore(void){
+    void CAN_recieve
+  }
+
+}
