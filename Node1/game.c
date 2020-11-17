bb97c8f4c0a6d5cd0c3030bb881b13b621a70c5d
EnGud <60407025+EnGud@users.noreply.github.com>
2020-11-17 11:25:20 +0100

    Update game.c

--- Node1/game.c
+++ Node1/game.c
@@ -1,4 +1,4 @@
-//this system is a WIP
+//this system is a WIP. The main functionality is there, but a lot of bugfixing might be needed.
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,19 +9,30 @@
 
 void setup(void){}
 
+int Score = 0;
 
 void main(void){
 //The main function for actually running the game. This function will loop for as long as score remains under 5. 
   void PlayGame(void){
     while(Game=="True"){
       can_send(joystick);
-      CAN_recieve(somestruct);
+      can_send(motor_pos);
+      
+      can_receive(ir_status); //receives data if IR is cut, which means a point is scored.  
+      
+      if(IR_Status == somevalue(high?number?){
+        Score++
+          if(score > 5){
+            Game == "False";
+          }
+      
+      }
         
     }
   }
   //a simple function to show the latest live high-score on the screen. This resets when the system resets.
   void ShowHighScore(void){
-    CAN_recieve(score_variable); 
+
       
       char ScoreAsChar = score_variable + '0';
     oled_print(ScoreAsChar);
