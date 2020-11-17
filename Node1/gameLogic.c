a5bab4f1d3ce7a6cd23740d2af144c88b4738c23
EnGud <60407025+EnGud@users.noreply.github.com>
2020-11-17 11:46:16 +0100

    Create gameLogic.c

---
+++ Node1/gameLogic.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define numberOfDebounce 3
+#define maxScore 5
+
+//This is a prototyping file with functions needed for game logic.
+int softwareDebounce = 0;
+int score = 0;
+
+int checkifIRBreaks(int IRValue, int IRThreshold) {
+	if (IRValue > IRThreshold) {
+		softwareDebounce++;
+		printf("We have registered a score, but will check up to %d times, now %d \n", numberOfDebounce, softwareDebounce);
+		if (softwareDebounce >= numberOfDebounce) {
+			softwareDebounce = 0;
+			return 0;
+			
+		}
+	}
+	else {
+		printf("nothing");
+		return 1;
+		softwareDebounce = 0;
+	}
+}
+
+int checkIfAlive(int maxScoreVariable, int IRValue, int IRThreshold) {
+	for (;;) {
+		while (score < maxScoreVariable) {
+			while (checkifIRBreaks(2,1)) {
+			}
+			score++;
+			oled_print("You have scored, and have %d points: \n", score);
+			if (score >= maxScoreVariable) {
+				oled_print("You have won, thanks for playing with us :-)!");
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+void main() {
+	while(checkIfAlive(maxScore)) {}
+}
