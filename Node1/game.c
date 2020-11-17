d069b21bb1da9f82f1f9aebfa95dfb6012345e60
EnGud <60407025+EnGud@users.noreply.github.com>
2020-11-17 11:11:48 +0100

    Update game.c

--- Node1/game.c
+++ Node1/game.c
@@ -8,17 +8,18 @@ void setup(void){}
 
 
 void main(void){
-
+//The main function for actually running the game. This function will loop for as long as score remains under 5. 
   void PlayGame(void){
     while(Game=="True"){
-      can_send(joystick)
-      CAN_recieve(somestruct)
+      can_send(joystick);
+      CAN_recieve(somestruct);
         
     }
   }
-  
+  //a simple function to show the latest live high-score on the screen. This resets when the system resets.
   void ShowHighScore(void){
-    void CAN_recieve
+    CAN_recieve(score_variable);?
+    oled_print(
   }
 
 }
